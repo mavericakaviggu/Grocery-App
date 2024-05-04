b95d1936e4efbc6447f72c72a88788848c9ac778
vignesh <vigneshshet4577@gamil.com>
2024-05-04 15:26:59 +0530

    updated db connection and table names

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -2,25 +2,25 @@ from sql_connection import get_sql_connection
 
 def get_all_products(connection):
     cursor = connection.cursor()
-    query = ("select products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name from products inner join uom on products.uom_id=uom.uom_id")
+    query = ("select products.product_id, products.name, products.unit, products.price_unit, unit_table.unitName from products inner join unit_table on products.unit=unit_table.unitId")
     cursor.execute(query)
     response = []
-    for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+    for (product_id, name, unit, price_unit, unitName) in cursor:
         response.append({
             'product_id': product_id,
             'name': name,
-            'uom_id': uom_id,
-            'price_per_unit': price_per_unit,
-            'uom_name': uom_name
+            'unit': unit,
+            'price_unit': price_unit,
+            'unitName': unitName
         })
     return response
 
 def insert_new_product(connection, product):
     cursor = connection.cursor()
     query = ("INSERT INTO products "
-             "(name, uom_id, price_per_unit)"
+             "(name, unit, price_unit)"
              "VALUES (%s, %s, %s)")
-    data = (product['product_name'], product['uom_id'], product['price_per_unit'])
+    data = (product['product_name'], product['unit'], product['price_unit'])
 
     cursor.execute(query, data)
     connection.commit()
@@ -40,6 +40,6 @@ if __name__ == '__main__':
     # print(get_all_products(connection))
     print(insert_new_product(connection, {
         'product_name': 'potatoes',
-        'uom_id': '1',
-        'price_per_unit': 10
+        'unit': '1',
+        'price_unit': 10
     }))
